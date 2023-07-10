a2d20735dbca280f06345abada7ecca8b608735c
CuencaJ <106718340+CuencaJ@users.noreply.github.com>
2023-07-10 03:39:49 -0500

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { IndexComponent } from './index/index.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { MensajenuevoComponent } from './mensajenuevo/mensajenuevo.component';
+import { MensajesComponent } from './mensajes/mensajes.component';
+import { NotificacionComponent } from './notificacion/notificacion.component';
+import { PerfilComponent } from './perfil/perfil.component';
+import { RecordatorioComponent } from './recordatorio/recordatorio.component';
+import { RegistroComponent } from './registro/registro.component';
+
+import { RecordatorionuevoComponent } from './recordatorionuevo/recordatorionuevo.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    IndexComponent,
+    InicioComponent,
+    MensajenuevoComponent,
+    MensajesComponent,
+    NotificacionComponent,
+    PerfilComponent,
+    RecordatorioComponent,
+    RegistroComponent,
+
+    RecordatorionuevoComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
